d50404152452ee6d68bd6b0b0480fd08cdbb0028
Daniel Nikolov <114946180+danikolovv@users.noreply.github.com>
2026-05-14 01:14:24 +0300

    Add files via upload

--- OOP1stPart_CourseProject_Daniel_Nikolov_24621705/src/FileService/FileService.java
+++ OOP1stPart_CourseProject_Daniel_Nikolov_24621705/src/FileService/FileService.java
@@ -1,4 +1,56 @@
 package FileService;
 
+import Loggers.FileOperationsLogger;
+
 public class FileService {
+    // Operation functions
+
+    // open
+
+    // save
+
+    // save as
+
+    // close
+    public void close() {
+
+
+        //FileOperationsLogger.closeLog(/*file name*/);
+    }
+
+    /**
+     * <span>
+     * Assembles string data that gets passed to corresponding log method for the operation.
+     * </span>
+     * <br><br>
+     * Params: (void)
+     * @return (void)
+     */
+    public void help() {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("The following commands are supported:").append("\n")
+                .append("open <file>").append("\t\t").append("opens <file>").append("\n")
+                .append("close").append("\t\t").append("closes currently opened file").append("\n")
+                .append("save").append("\t\t").append("saves the currently open file").append("\n")
+                .append("save as <file>").append("\t\t").append("saves the currently open file in <file>").append("\n")
+                .append("help").append("\t\t").append("prints this information").append("\n")
+                .append("exit").append("\t\t").append("exits the program").append("\n");
+
+        FileOperationsLogger.helpLog(sb.toString());
+    }
+
+    /**
+     * <span>
+     * Calls System.exit(exitStatus) method for JVM termination i.e. program execution termination
+     * </span>
+     * <br><br>
+     * Params: (void)
+     * @return (void)
+     */
+    public void exit() {
+        FileOperationsLogger.exitLog();
+        System.exit(0);     // not abnormal termination sequence initiation of JVM
+    }
+
 }
---
+++ OOP1stPart_CourseProject_Daniel_Nikolov_24621705/src/Loggers/FileOperationsLogger.java
@@ -0,0 +1,41 @@
+package Loggers;
+
+public class FileOperationsLogger {
+
+    /**
+     * <span>
+     * Executes System.out.println() method -> File operation help() logging
+     * </span>
+     * <br><br>
+     * @param stringData data that help() should inform the user with
+     * @return (void)
+     */
+    public static void helpLog(String stringData) {
+        System.out.println(stringData);
+    }
+
+    /**
+     * <span>
+     * Executes System.out.println() method -> File operation close() logging
+     * </span>
+     * <br><br>
+     * @param fileName name of the file to be contained in the output
+     * @return (void)
+     */
+    public static void closeLog(String fileName) {
+        System.out.println(fileName);
+    }
+
+    /**
+     * <span>
+     * Executes System.out.println() method -> File operation exit() logging
+     * </span>
+     * <br><br>
+     * Params: (void)
+     * @return (void)
+     */
+    public static void exitLog() {
+        System.out.println("Exiting the program...");
+    }
+
+}
