f13fce8d839e886df753f826ecf092db5cf60f1a
Daniel Nikolov <114946180+danikolovv@users.noreply.github.com>
2026-05-18 03:20:39 +0300

    Delete OOP1stPart_CourseProject_Daniel_Nikolov_24621705/src/CFGFileOperations.java

--- OOP1stPart_CourseProject_Daniel_Nikolov_24621705/src/CFGFileOperations.java
+++
@@ -1,8 +0,0 @@
-package cfgService.operations;
-
-public class CFGFileOperations {
-
-    public static void save(int id, String filename) {
-        // implement file saving of the Grammar
-    }
-}
